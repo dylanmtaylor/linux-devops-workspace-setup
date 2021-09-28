c39c9dc1666474c0f8f0cd9c8d7df02fdcbd62d9
Dylan M. Taylor <dylan@dylanmtaylor.com>
2021-09-28 12:05:07 -0400

    Add powershell

--- workspace-setup.sh
+++ workspace-setup.sh
@@ -5,8 +5,9 @@ sudo yum -y update
 sudo curl -o /etc/yum.repos.d/konimex-neofetch-epel-7.repo https://copr.fedorainfracloud.org/coprs/konimex/neofetch/repo/epel-7/konimex-neofetch-epel-7.repo
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+curl https://packages.microsoft.com/config/rhel/7/prod.repo | sudo tee /etc/yum.repos.d/microsoft.repo
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-sudo yum -y install chromium gimp docker meld neofetch htop code terraform packer vault vagrant consul nomad boundary waypoint ansible remmina wireshark-gnome go
+sudo yum -y install chromium gimp docker meld neofetch htop code terraform packer vault vagrant consul nomad boundary waypoint ansible remmina wireshark-gnome go powershell
 # TODO: Install GitKraken here
 sudo systemctl enable --now docker
 curl https://intoli.com/install-google-chrome.sh | sudo bash
