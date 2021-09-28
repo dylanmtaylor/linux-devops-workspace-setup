9be842cc803a5b6c9c3951bc89025a1caffc8349
Dylan M. Taylor <dylan@dylanmtaylor.com>
2021-09-28 11:49:38 -0400

    Just install all of the HashiCorp products; might use them

--- workspace-setup.sh
+++ workspace-setup.sh
@@ -6,7 +6,7 @@ sudo curl -o /etc/yum.repos.d/konimex-neofetch-epel-7.repo https://copr.fedorain
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-sudo yum -y install chromium gimp docker meld neofetch code terraform packer vault vagrant ansible
+sudo yum -y install chromium gimp docker meld neofetch code terraform packer vault vagrant consul nomad boundary waypoint ansible
 # TODO: Install GitKraken here
 sudo systemctl enable --now docker
 curl https://intoli.com/install-google-chrome.sh | sudo bash
