020e52ccaa15f1c331d2a86e98a21a15a1a4e537
Dylan M. Taylor <dylan@dylanmtaylor.com>
2021-09-28 12:01:00 -0400

    Add multiple more things that are useful for development

--- workspace-setup.sh
+++ workspace-setup.sh
@@ -1,13 +1,15 @@
 ## This was originally written for my personal use, in case I recreate my workspace but others may find this valuable. Free to use or modify however you want.
 ## Tested on Standard Amazon Linux 2 Workspace
-sudo amazon-linux-extras install epel kernel-ng
+sudo amazon-linux-extras install epel kernel-ng ruby3.0 python3.8 java-openjdk11
 sudo yum -y update
 sudo curl -o /etc/yum.repos.d/konimex-neofetch-epel-7.repo https://copr.fedorainfracloud.org/coprs/konimex/neofetch/repo/epel-7/konimex-neofetch-epel-7.repo
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-sudo yum -y install chromium gimp docker meld neofetch code terraform packer vault vagrant consul nomad boundary waypoint ansible remmina
+sudo yum -y install chromium gimp docker meld neofetch htop code terraform packer vault vagrant consul nomad boundary waypoint ansible remmina wireshark-gnome go
 # TODO: Install GitKraken here
 sudo systemctl enable --now docker
 curl https://intoli.com/install-google-chrome.sh | sudo bash
+sudo yum -y install rubygems ruby-devel rubygem-rspec gcc gcc-c++
+gem install serverspec
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
