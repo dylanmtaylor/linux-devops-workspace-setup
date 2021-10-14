4354708b94a6d3995686975ac0d7ff6d8e98097a
Dylan Taylor <dylan@dylanmtaylor.com>
2021-10-13 20:04:17 -0400

    Add VLC flatpak version

--- workspace-setup.sh
+++ workspace-setup.sh
@@ -29,3 +29,4 @@ aws --version
 sudo yum -y install flatpak
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo flatpak install flathub io.dbeaver.DBeaverCommunity -y
+sudo flatpak install flathub org.videolan.VLC -y
