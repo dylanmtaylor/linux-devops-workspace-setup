f47d99944f4af2eaae52cf67f693575663f3ebe3
Dylan M. Taylor <dylan@dylanmtaylor.com>
2022-09-29 19:18:07 -0400

    Add a few more

--- ubuntu/workspace-setup.sh
+++ ubuntu/workspace-setup.sh
@@ -2,7 +2,7 @@
 
 # Google Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt -y install ./google-chrome-stable_current_amd64.deb unzip p7zip-full wget gpg flatpak build-essential
+sudo apt -y install ./google-chrome-stable_current_amd64.deb unzip p7zip-full curl wget gpg flatpak build-essential
 rm -f ./google-chrome-stable_current_amd64.deb
 
 # VS Code
@@ -11,6 +11,9 @@ sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 rm -f packages.microsoft.gpg
 sudo apt -y install code
+
+# Oh My ZSH
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Docker
 curl -fsSL https://get.docker.com | bash
@@ -37,8 +40,11 @@ sudo apt -y install meld btop htop remmina neofetch nmap ncat wireshark-gtk tcpd
 # Image editing and media
 sudo apt -y install krita gimp inkscape pinta vlc obs-studio
 
-# Development tools: OpenJDK 11 and NodeJS
-sudo apt -y install openjdk-11-jdk nodejs cargo yarn
+# Development tools: OpenJDK 11, Rust and NodeJS, etc.
+sudo apt -y install openjdk-11-jdk nodejs cargo yarn maven ansible powershell go python3-pip neovim 
+
+# Topgrade for easy system upgrades
+cargo install topgrade
 
 # Do a full upgrade
 sudo apt update && sudo apt -y full-upgrade
