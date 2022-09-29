e661783127eb575a311ceb627bbe4a2ae347b687
Dylan M. Taylor <dylan@dylanmtaylor.com>
2022-09-29 19:21:29 -0400

    Fix failure to fetch tools

--- ubuntu/workspace-setup.sh
+++ ubuntu/workspace-setup.sh
@@ -10,7 +10,12 @@ wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > pa
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 rm -f packages.microsoft.gpg
-sudo apt -y install code
+sudo apt update && sudo apt -y install code
+
+# Hashicorp Tools
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt -y install terraform packer vault vagrant consul nomad boundary waypoint
 
 # Oh My ZSH
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -28,11 +33,6 @@ sudo snap install dbeaver-ce
 
 # Postman
 sudo snap install postman
-
-# Hashicorp Tools
-wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt -y install terraform packer vault vagrant consul nomad boundary waypoint
 
 # Various networking and monitoring tools
 sudo apt -y install meld btop htop remmina neofetch nmap ncat wireshark-gtk tcpdump filezilla ghex
