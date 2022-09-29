5cdbb078b4773423a9ba4709d40616842a7465c6
Dylan M. Taylor <dylan@dylanmtaylor.com>
2022-09-29 18:39:22 -0400

    Begin porting my workspace to Ubuntu 22.04

---
+++ ubuntu/workspace-setup.sh
@@ -0,0 +1,4 @@
+rm *.deb
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb unzip -y
+curl -fsSL https://get.docker.com | bash
