b8d560edd7904e7b1d9ae2e911808f5c55c275e5
Dylan M. Taylor <dylan@dylanmtaylor.com>
2022-09-29 19:34:34 -0400

    Fix Hashicorp tooling

--- ubuntu/workspace-setup.sh
+++ ubuntu/workspace-setup.sh
@@ -13,7 +13,7 @@ rm -f packages.microsoft.gpg
 sudo apt update && sudo apt -y install code
 
 # Hashicorp Tools
-wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+wget --quiet --output-document - https://apt.releases.hashicorp.com/gpg | \ sudo gpg --no-default-keyring --keyring gnupg-ring:/usr/share/keyrings/hashicorp-archive-keyring.gpg --import
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update && sudo apt -y install terraform packer vault vagrant consul nomad boundary waypoint
 
