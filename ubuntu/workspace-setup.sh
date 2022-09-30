40c5217dfccb20896c4106fb3d1d8a4448705b3a
Dylan M. Taylor <dylan@dylanmtaylor.com>
2022-09-29 20:57:37 -0400

    yqq made it look like nothing was happening

--- ubuntu/workspace-setup.sh
+++ ubuntu/workspace-setup.sh
@@ -6,11 +6,11 @@ export NEEDRESTART_SUSPEND=true
 export DEBIAN_PRIORITY=critical
 
 # Do a system upgrade
-sudo apt update && sudo apt -yqq full-upgrade
+sudo apt update && sudo apt -y full-upgrade
 
 # Google Chrome (and some various packages that are dependencies)
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt update && sudo apt -yqq install ./google-chrome-stable_current_amd64.deb unzip p7zip-full curl wget gpg flatpak build-essential zsh
+sudo apt update && sudo apt -y install ./google-chrome-stable_current_amd64.deb unzip p7zip-full curl wget gpg flatpak build-essential zsh
 rm -f ./google-chrome-stable_current_amd64.deb
 
 # VS Code
@@ -18,16 +18,16 @@ wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > pa
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 rm -f packages.microsoft.gpg
-sudo apt update && sudo apt -yqq install code
+sudo apt update && sudo apt -y install code
 
 # Various networking and monitoring tools
-sudo apt -yqq install meld btop htop remmina neofetch nmap ncat wireshark-gtk tcpdump filezilla ghex
+sudo apt -y install meld btop htop remmina neofetch nmap ncat wireshark-gtk tcpdump filezilla ghex
 
 # Image editing and media
-sudo apt -yqq install krita gimp inkscape pinta vlc obs-studio shutter
+sudo apt -y install krita gimp inkscape pinta vlc obs-studio shutter
 
 # Development tools: OpenJDK 11, Rust and NodeJS, etc.
-sudo apt -yqq install openjdk-11-jdk nodejs cargo yarn maven ansible golang python3-pip neovim 
+sudo apt -y install openjdk-11-jdk nodejs cargo yarn maven ansible golang python3-pip neovim 
 
 
 # GitKraken
@@ -57,7 +57,7 @@ sudo systemctl enable --now docker
 # Hashicorp Tools
 wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update && sudo apt -yqq install boundary consul consul-esm consul-k8s consul-template consul-terraform-sync nomad nomad-autoscaler packer terraform terraform-ls vagrant vault waypoint
+sudo apt update && sudo apt -y install boundary consul consul-esm consul-k8s consul-template consul-terraform-sync nomad nomad-autoscaler packer terraform terraform-ls vagrant vault waypoint
 
 # Topgrade for easy system upgrades
 cargo install topgrade
