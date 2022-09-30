3cb26cc61a16f6c1dba0525c07c1ccf24e1b90b2
Dylan M. Taylor <dylan@dylanmtaylor.com>
2022-09-29 20:46:21 -0400

    Re-organize installs putting more problem-prone ones at the end.

--- ubuntu/workspace-setup.sh
+++ ubuntu/workspace-setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+export DEBIAN_FRONTEND=noninteractive
+
+# Do a system upgrade
+sudo apt update && sudo apt -y upgrade
+
 # Google Chrome (and some various packages that are dependencies)
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt update && sudo apt -y install ./google-chrome-stable_current_amd64.deb unzip p7zip-full curl wget gpg flatpak build-essential zsh
@@ -12,18 +17,15 @@ sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packag
 rm -f packages.microsoft.gpg
 sudo apt update && sudo apt -y install code
 
-# Hashicorp Tools
-wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update && sudo apt -y install boundary consul consul-esm consul-k8s consul-template consul-terraform-sync nomad nomad-autoscaler packer terraform terraform-ls vagrant vault waypoint
+# Various networking and monitoring tools
+sudo apt -y install meld btop htop remmina neofetch nmap ncat wireshark-gtk tcpdump filezilla ghex
 
-# Oh My ZSH
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Image editing and media
+sudo apt -y install krita gimp inkscape pinta vlc obs-studio shutter
 
-# Docker
-curl -fsSL https://get.docker.com | bash
-sudo usermod -aG docker $USER
-sudo systemctl enable --now docker
+# Development tools: OpenJDK 11, Rust and NodeJS, etc.
+sudo apt -y install openjdk-11-jdk nodejs cargo yarn maven ansible golang python3-pip neovim 
+
 
 # GitKraken
 sudo snap install gitkraken --classic
@@ -37,21 +39,22 @@ sudo snap install postman
 # PowerShell
 sudo snap install powershell --classic
 
-# Various networking and monitoring tools
-sudo apt -y install meld btop htop remmina neofetch nmap ncat wireshark-gtk tcpdump filezilla ghex
-
-# Image editing and media
-sudo apt -y install krita gimp inkscape pinta vlc obs-studio shutter
-
-# Development tools: OpenJDK 11, Rust and NodeJS, etc.
-sudo apt -y install openjdk-11-jdk nodejs cargo yarn maven ansible golang python3-pip neovim 
-
 # JetBrains community IDEs
 sudo snap install pycharm-community --classic
 sudo snap install intellij-idea-community --classic --edge
 
+# Oh My ZSH
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Docker
+curl -fsSL https://get.docker.com | bash
+sudo usermod -aG docker $USER
+sudo systemctl enable --now docker
+
+# Hashicorp Tools
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt -y install boundary consul consul-esm consul-k8s consul-template consul-terraform-sync nomad nomad-autoscaler packer terraform terraform-ls vagrant vault waypoint
+
 # Topgrade for easy system upgrades
 cargo install topgrade
-
-# Do a full upgrade
-sudo apt update && sudo apt -y full-upgrade
