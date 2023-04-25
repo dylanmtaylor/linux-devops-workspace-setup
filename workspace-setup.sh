e5bf173a0a25bf435832be109ec461d933d0b20d
Dylan M. Taylor <dylan@dylanmtaylor.com>
2023-04-25 08:48:18 -0400

    Install the real 7-zip, not just p7zip-full

--- workspace-setup.sh
+++ workspace-setup.sh
@@ -7,7 +7,7 @@ export DEBIAN_PRIORITY=critical
 
 # Do a system upgrade and install some pre-reqs plus GNOME packages for further system customization
 sudo -E apt update && sudo -E apt -y full-upgrade
-sudo -E apt -y install unzip p7zip-full curl wget gpg flatpak gnome-software-plugin-flatpak chrome-gnome-shell gnome-tweaks gnome-shell-extension-manager gnome-boxes build-essential zsh libfuse2
+sudo -E apt -y install unzip unrar 7zip p7zip-full curl wget gpg flatpak gnome-software-plugin-flatpak chrome-gnome-shell gnome-tweaks gnome-shell-extension-manager gnome-boxes build-essential zsh libfuse2
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Install Flatpak updates if any are available
