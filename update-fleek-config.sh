d21426254e621fe5205d356cf6938ec10d08ffe6
Dylan M. Taylor <dylan@dylanmtaylor.com>
2024-02-23 21:12:26 -0500

    Update update-fleek-config.sh

--- update-fleek-config.sh
+++ update-fleek-config.sh
@@ -2,5 +2,7 @@
 curl -L https://raw.githubusercontent.com/dylanmtaylor/amazon-linux-devops-workspace-setup/main/.fleek.yml > $HOME/.local/share/fleek/.fleek.yml
 sed -i "s/dylantaylor-pc/$(hostname)/g" $HOME/.local/share/fleek/.fleek.yml
 sed -i "s/username: dylan/username: $USER/g" $HOME/.local/share/fleek/.fleek.yml
-fleek update
-fleek apply
+rm $HOME/.fleek.yaml
+ln -s $HOME/.local/share/fleek/.fleek.yml $HOME/.fleek.yaml
+nix run "https://getfleek.dev/latest.tar.gz" -- update
+nix run "https://getfleek.dev/latest.tar.gz" -- apply
